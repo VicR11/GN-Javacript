5b010bafed2e1109a22d42da766c3e8f95817307
VicR11 <11victoriareyes@gmail.com>
2025-04-23 23:40:33 -0500

    Se mejora la logica y se agregan estilos

--- LabManejoDOM/script.js
+++ LabManejoDOM/script.js
@@ -7,32 +7,54 @@ let botonAgregar = document.getElementById("agregar")
 
 botonAgregar.addEventListener("click", function(event) {
     event.preventDefault(); 
-    //lista.innerHTML += `<li>${tarea.value} <button class="EliminarTarea">Eliminar</button></li>`; 
-    //tarea.value ="";
-    
-    let elemento = document.createElement("li")
-    elemento.textContent = tarea.value;
+    lista.innerHTML += `<li>
+                            <label>
+                            <input type="checkbox">
+                            ${tarea.value}
+                            </label>
+                            <button class="EliminarTarea">Eliminar</button>
+                        </li>` 
+    tarea.value ="";
 
-    let checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-
-    let botonEliminar = document.createElement("button");
-    botonEliminar.textContent = "Eliminar";
-    botonEliminar.classList.add("EliminarTarea");
-
-    
-
-    elemento.appendChild(checkbox)
-    elemento.appendChild(botonEliminar);
-    lista.appendChild(elemento)
-
-    botonEliminar.addEventListener("click", function() {
-        elemento.remove(); 
-    });
-
-    console.log(elemento);
+    /*let botonEliminarList = document.querySelectorAll(".EliminarTarea")
+        botonEliminarList.forEach(botonEliminar =>{
+            botonEliminar.addEventListener("click", function(event) {
+                console.log(botonEliminar);
+                botonEliminar.parentElement.remove();
+                
+            })
+        })*/
+    console.log(lista);
     
 })
+
+lista.addEventListener("click", function(event) {
+     //event.target --> toma el elemento 
+   if (event.target.classList.contains("EliminarTarea")) {
+        event.target.parentElement.remove();
+    }
+});
+
+
+lista.addEventListener("click", function(event) {
+    //En este caso checked es un atributo del input porque eso se accede directamente 
+    if(event.target.checked){
+        event.target.parentElement.classList.toggle("checked")
+}
+
+    
+    
+
+
+});
+
+
+
+
+
+
+
+
 
 
 
